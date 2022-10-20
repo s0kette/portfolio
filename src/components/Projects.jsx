7e9ceb8dd250f1c0faa108258da2aeca34402b28
s0kette <timothy.perkins.skr@gmail.com>
2022-10-21 01:11:04 +0200

    adds social medias

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -7,28 +7,28 @@ function Projects() {
         <h2>Mes derniers projets</h2>
         <div className='website-thumbnail'>
             <div className='netflix'>
-                <a className='link' href="https://netflix-nextjs-six.vercel.app/login">
+                <a className='link' href="https://netflix-nextjs-six.vercel.app/login" target="_blank" rel="noreferrer">
                     <img src="./img/Netflix.png" alt="" />
                     <p>Reproduction de Netflix, avec une inscription/connexion</p>
                 </a>
             </div>
 
             <div className='guessthequotes'>
-                <a className='link' href="https://guessthequotes.vercel.app/">
+                <a className='link' href="https://guessthequotes.vercel.app/" target="_blank" rel="noreferrer">
                     <img src="./img/GuesstheQuotes.png" alt="" />
                     <p>Prototype d'un quiz sur le cinéma</p>
                 </a>
             </div>
 
             <div className='frenchcuts'>
-                <a className='link' href="https://www.french-cuts.com">
+                <a className='link' href="https://www.french-cuts.com" target="_blank" rel="noreferrer">
                     <img src="./img/FrenchCuts.png" alt="" />
                     <p>Site vitrine d'un barbier à Chicago</p>
                 </a>
             </div>
 
             <div className='grandspirit'>
-                <a className='link' href="https://grandspirit.vercel.app/">
+                <a className='link' href="https://grandspirit.vercel.app/" target="_blank" rel="noreferrer">
                     <img src="./img/GrandSpirit.png" alt="" />
                     <p>Site vitrine d'une équipe e-sportive</p>
                 </a>
